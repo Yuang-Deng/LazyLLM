c5e57479ca5ecc2ca7b08b61a994117cae265e13
wangjian2 <wangjian2@sensetime.com>
2025-09-08 11:38:01 +0800

    Added XMLReader and XMLSplitter functions, mainly for Chengfei's S1000D format, but currently expected to adapt to the general XML format

--- lazyllm/tools/rag/__init__.py
+++ lazyllm/tools/rag/__init__.py
@@ -2,10 +2,11 @@ from .document import Document
 from .retriever import Retriever, TempDocRetriever
 from .rerank import Reranker, register_reranker
 from .transform import SentenceSplitter, LLMParser, NodeTransform, TransformArgs, AdaptiveTransform
+from .xml_transform import XMLSplitter
 from .similarity import register_similarity
 from .doc_node import DocNode
 from .readers import (PDFReader, DocxReader, HWPReader, PPTXReader, ImageReader, IPYNBReader, EpubReader,
-                      MarkdownReader, MboxReader, PandasCSVReader, PandasExcelReader, VideoAudioReader)
+                      MarkdownReader, MboxReader, PandasCSVReader, PandasExcelReader, VideoAudioReader, XMLReader)
 from .dataReader import SimpleDirectoryReader, FileReader
 from .doc_manager import DocManager, DocListManager
 from .global_metadata import GlobalMetadataDesc as DocField
@@ -23,6 +24,7 @@ __all__ = [
     "TransformArgs",
     "SentenceSplitter",
     "LLMParser",
+    "XMLSplitter",
     "register_similarity",
     "register_reranker",
     "DocNode",
@@ -39,6 +41,7 @@ __all__ = [
     "PandasExcelReader",
     "VideoAudioReader",
     "SimpleDirectoryReader",
+    "XMLReader",
     'DocManager',
     'DocListManager',
     'DocField',
